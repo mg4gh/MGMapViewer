2b234251d6d7d9ab0096cc3f3d38638f65a9c874
mg4gh <mgroeger1@web.de>
2020-11-26 09:27:10 +0100

    fix debug level for MultiPointView - too much log entries otherwise

--- mgmap/src/main/java/mg/mapviewer/view/MultiPointView.java
+++ mgmap/src/main/java/mg/mapviewer/view/MultiPointView.java
@@ -141,7 +141,7 @@ public class MultiPointView extends MVLayer {
 
     private void drawPoint(Canvas canvas, int x, int y, byte zoomLevel){
         if (pointRadius > 0){
-            if (showIntermediates) Log.i(MGMapApplication.LABEL, NameUtil.context()+" r="+(int)(pointRadius* getScale(zoomLevel))+" zoom="+zoomLevel+" sw="+(paintStroke.getStrokeWidth() * getScale(zoomLevel)));
+            if (showIntermediates) Log.v(MGMapApplication.LABEL, NameUtil.context()+" r="+(int)(pointRadius* getScale(zoomLevel))+" zoom="+zoomLevel+" sw="+(paintStroke.getStrokeWidth() * getScale(zoomLevel)));
             canvas.drawCircle(x, y, (int)(pointRadius* getScale(zoomLevel)), this.paintStroke);
         }
     }
