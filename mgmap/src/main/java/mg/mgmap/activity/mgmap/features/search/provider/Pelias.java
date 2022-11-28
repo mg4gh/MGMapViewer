c0647348c441b9a45818c67f56fa3989b707b9fd
mg4gh <mg4gh@web.de>
2022-11-28 22:25:11 +0100

    rework warnings in Pelias

--- mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/Pelias.java
+++ mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/Pelias.java
@@ -23,7 +23,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Properties;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -41,7 +40,6 @@ import mg.mgmap.activity.mgmap.features.search.SearchView;
 import mg.mgmap.generic.model.PointModel;
 import mg.mgmap.generic.model.PointModelImpl;
 import mg.mgmap.generic.util.basic.NameUtil;
-import mg.mgmap.application.util.PersistenceManager;
 
 public class Pelias extends SearchProvider {
 
@@ -73,59 +71,56 @@ public class Pelias extends SearchProvider {
         PointModel pm = request.pos;
         int radius = 50 << Math.max(0,  10 - request.zoom);
 
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    ArrayList<SearchResult> resList = new ArrayList<>();
+        new Thread(() -> {
+            try {
+                ArrayList<SearchResult> resList = new ArrayList<>();
 
-                    String sUrl;
-                    if (request.text.equals("")){
-                        sUrl = String.format(Locale.ENGLISH, "%sreverse?api_key=%s&size=5&point.lon=%.6f&point.lat=%.6f",
-                                URL_ORS, apiKey, pm.getLon(), pm.getLat());
-                    } else {
-                        sUrl = String.format(Locale.ENGLISH, "%sautocomplete?api_key=%s&text=%s&size=5&focus.point.lon=%.6f&focus.point.lat=%.6f&boundary.circle.lon=%.6f&boundary.circle.lat=%.6f&boundary.circle.radius=%d",
-                                URL_ORS, apiKey, request.text, pm.getLon(), pm.getLat(), pm.getLon(), pm.getLat(), radius);
-                    }
-                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+sUrl);
-                    URL url = new URL(sUrl);
-                    URLConnection conn = url.openConnection();
-                    InputStream is = conn.getInputStream();
+                String sUrl;
+                if (request.text.equals("")){
+                    sUrl = String.format(Locale.ENGLISH, "%sreverse?api_key=%s&size=5&point.lon=%.6f&point.lat=%.6f",
+                            URL_ORS, apiKey, pm.getLon(), pm.getLat());
+                } else {
+                    sUrl = String.format(Locale.ENGLISH, "%sautocomplete?api_key=%s&text=%s&size=5&focus.point.lon=%.6f&focus.point.lat=%.6f&boundary.circle.lon=%.6f&boundary.circle.lat=%.6f&boundary.circle.radius=%d",
+                            URL_ORS, apiKey, request.text, pm.getLon(), pm.getLat(), pm.getLon(), pm.getLat(), radius);
+                }
+                Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+sUrl);
+                URL url = new URL(sUrl);
+                URLConnection conn = url.openConnection();
+                InputStream is = conn.getInputStream();
 
-                    JsonReader jsonReader = Json.createReader(is);
-                    JsonObject oAll = jsonReader.readObject();
+                JsonReader jsonReader = Json.createReader(is);
+                JsonObject oAll = jsonReader.readObject();
 
-                    JsonArray fAll = oAll.getJsonArray("features");
-                    for (JsonValue f : fAll) {
+                JsonArray fAll = oAll.getJsonArray("features");
+                for (JsonValue f : fAll) {
 
-                        try {
-                            JsonObject fo = f.asJsonObject();
+                    try {
+                        JsonObject fo = f.asJsonObject();
 
-                            JsonObject go = fo.getJsonObject("geometry");
-                            JsonArray coos = go.getJsonArray("coordinates");
+                        JsonObject go = fo.getJsonObject("geometry");
+                        JsonArray coos = go.getJsonArray("coordinates");
 
-                            double lon = coos.getJsonNumber(0).doubleValue();
-                            double lat = coos.getJsonNumber(1).doubleValue();
-                            PointModel pm = new PointModelImpl(lat,lon);
+                        double lon = coos.getJsonNumber(0).doubleValue();
+                        double lat = coos.getJsonNumber(1).doubleValue();
+                        PointModel pm1 = new PointModelImpl(lat,lon);
 
-                            JsonObject po = fo.getJsonObject("properties");
-                            String resText = String.format("%s", po.getString("label"));
+                        JsonObject po = fo.getJsonObject("properties");
+                        String resText = String.format("%s", po.getString("label"));
 
-                            resList.add( new SearchResult(request, resText, pm));
-                            Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+resText);
-                        } catch (Exception e) {
-                            Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
-                        }
-
+                        resList.add( new SearchResult(request, resText, pm1));
+                        Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+resText);
+                    } catch (Exception e) {
+                        Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
                     }
 
-                    publishResult(request, resList);
-                } catch (IOException e) {
-                    Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
                 }
 
+                publishResult(request, resList);
+            } catch (IOException e) {
+                Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
             }
-        }.start();
+
+        }).start();
     }
 
 
