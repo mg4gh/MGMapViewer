2c9faabb00b142bea28aa79655b41db747446d53
mg4gh <mg4gh@web.de>
2022-11-28 22:26:32 +0100

    rework warnings in POI

--- mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/POI.java
+++ mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/POI.java
@@ -106,123 +106,119 @@ public class POI extends SearchProvider {
         }
 
 
-        new Thread(){
-            @Override
-            public void run() {
-                SQLiteDatabase db = null;
-                try {
-                    TreeSet<SearchResult> resList = new TreeSet<>();
-                    HashMap<String, String> subMap = new HashMap<>();
+        new Thread(() -> {
+            SQLiteDatabase db = null;
+            try {
+                TreeSet<SearchResult> resList = new TreeSet<>();
+                HashMap<String, String> subMap = new HashMap<>();
 
-                    db = SQLiteDatabase.openDatabase(poiFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+                db = SQLiteDatabase.openDatabase(poiFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
 
-                    String select = "SELECT poi_data.id,poi_data.data,poi_index.id,poi_index.minLat,poi_index.maxLat,poi_index.minLon,poi_index.maxLon FROM poi_data INNER JOIN poi_index ON poi_data.id=poi_index.id  ";
-                    String posMatch = String.format(Locale.ENGLISH, " ( (minLat>%.6f) AND (minLon>%.6f) AND (maxLat<%.6f) AND (maxLon<%.6f) )",
-                            bBox.minLatitude,bBox.minLongitude,bBox.maxLatitude,bBox.maxLongitude);
-                    String textPart[] = request.text.split(" ");
-                    String textMatch = " AND ";
-                    for (String part : textPart){
-                        textMatch += " instr(lower(data),lower(\""+part+"\")) AND";
-                    }
-                    textMatch = textMatch.replaceAll("AND$", "");
-                    if (request.text.equals("")){
-                        textMatch = "";
-                    }
+                String select = "SELECT poi_data.id,poi_data.data,poi_index.id,poi_index.minLat,poi_index.maxLat,poi_index.minLon,poi_index.maxLon FROM poi_data INNER JOIN poi_index ON poi_data.id=poi_index.id  ";
+                String posMatch = String.format(Locale.ENGLISH, " ( (minLat>%.6f) AND (minLon>%.6f) AND (maxLat<%.6f) AND (maxLon<%.6f) )",
+                        bBox.minLatitude,bBox.minLongitude,bBox.maxLatitude,bBox.maxLongitude);
+                String[] textPart = request.text.split(" ");
+                StringBuilder textMatch = new StringBuilder(" AND ");
+                for (String part : textPart){
+                    textMatch.append(" instr(lower(data),lower(\"").append(part).append("\")) AND");
+                }
+                textMatch = new StringBuilder(textMatch.toString().replaceAll("AND$", ""));
+                if (request.text.equals("")){
+                    textMatch = new StringBuilder();
+                }
 
-                    String SELECT_STATEMENT = select + " WHERE ( " +posMatch+textMatch +");";
+                String SELECT_STATEMENT = select + " WHERE ( " +posMatch+textMatch +");";
 //                    String SELECT_STATEMENT = "SELECT * FROM poi_data INNER JOIN poi_index ON poi_data.id=poi_index.id WHERE  (instr(data,\""+request.text+"\"))";
-                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+SELECT_STATEMENT);
+                Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+SELECT_STATEMENT);
 
-                    Cursor cursor = null;
-                    cursor = db.rawQuery(SELECT_STATEMENT, null);
-                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" cursor.count="+cursor.getCount());
-                    while (cursor.moveToNext()) {
-                        // Column values
-                        int id = cursor.getInt(0);
-                        String text = cursor.getString(1);
-                        Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+id+" "+text);
+                Cursor cursor = db.rawQuery(SELECT_STATEMENT, null);
+                Log.i(MGMapApplication.LABEL, NameUtil.context()+" cursor.count="+cursor.getCount());
+                while (cursor.moveToNext()) {
+                    // Column values
+                    int id = cursor.getInt(0);
+                    String text = cursor.getString(1);
+                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+id+" "+text);
 
-                        subMap.clear();
-                        for (String resPart : text.split("\\r")){
+                    subMap.clear();
+                    for (String resPart : text.split("\\r")){
 //                            Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+resPart);
-                            String[] sub = resPart.split("=");
-                            if (sub.length == 2){
-                                subMap.put(sub[0],sub[1]);
-                            }
+                        String[] sub = resPart.split("=");
+                        if (sub.length == 2){
+                            subMap.put(sub[0],sub[1]);
                         }
-                        String res = "";
-                        String key;
+                    }
+                    String res = "";
+                    String key;
 
-                        if (("yes".equals(subMap.get("bus"))) || ("bus_stop".equals(subMap.get("highway")))){
-                            res += "Bus ";
-                        }
-                        if (("yes".equals(subMap.get("tram"))) || ("tram_stop".equals(subMap.get("railway")))){
-                            res += "Tram ";
-                        }
-                        if ("platform".equals(subMap.get("railway"))){
-                            continue;
-                        }
+                    if (("yes".equals(subMap.get("bus"))) || ("bus_stop".equals(subMap.get("highway")))){
+                        res += "Bus ";
+                    }
+                    if (("yes".equals(subMap.get("tram"))) || ("tram_stop".equals(subMap.get("railway")))){
+                        res += "Tram ";
+                    }
+                    if ("platform".equals(subMap.get("railway"))){
+                        continue;
+                    }
 
 
-                        key="name";
+                    key="name";
+                    if (subMap.containsKey(key)){
+                        res += subMap.get(key);
+                    } else {
+                        key="brand";
                         if (subMap.containsKey(key)){
                             res += subMap.get(key);
                         } else {
-                            key="brand";
-                            if (subMap.containsKey(key)){
-                                res += subMap.get(key);
-                            } else {
-                                continue;
-                            }
+                            continue;
                         }
-
-                        key="addr:street";
-                        res += (!subMap.containsKey(key))?"":", "+subMap.get(key);
-
-                        key="addr:housenumber";
-                        res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
-
-                        res += ",";
-                        key="addr:postcode";
-                        res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
-
-                        key="addr:city";
-                        res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
-
-                        if (res.endsWith(",")){
-                            res = res.substring(0,res.length()-1);
-                        }
-                        if (res.length() < 5) continue;
-
-
-
-
-                        double lat = cursor.getDouble(3);
-                        double lon = cursor.getDouble(5);
-                        PointModel pos = new PointModelImpl(lat,lon);
-                        Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+id+" "+pos+" "+res);
-
-                        SearchResult sr = new SearchResult(request, res, pos);
-                        sr.longResultText = text.replaceAll("\\r"," ");
-                        resList.add( sr );
                     }
 
-                    ArrayList<SearchResult> res = new ArrayList<>();
-                    for (SearchResult sr: resList) {
-                        res.add(sr);
-                        if (res.size() >= 5) break;
+                    key="addr:street";
+                    res += (!subMap.containsKey(key))?"":", "+subMap.get(key);
+
+                    key="addr:housenumber";
+                    res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
+
+                    res += ",";
+                    key="addr:postcode";
+                    res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
+
+                    key="addr:city";
+                    res += (!subMap.containsKey(key))?"":" "+subMap.get(key);
+
+                    if (res.endsWith(",")){
+                        res = res.substring(0,res.length()-1);
                     }
-                    publishResult(request, res);
-                } catch (Exception e) {
-                    Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
-                } finally {
-                    if (db != null){
-                        db.close();
-                    }
+                    if (res.length() < 5) continue;
+
+
+
+
+                    double lat = cursor.getDouble(3);
+                    double lon = cursor.getDouble(5);
+                    PointModel pos = new PointModelImpl(lat,lon);
+                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+id+" "+pos+" "+res);
+
+                    SearchResult sr = new SearchResult(request, res, pos);
+                    sr.longResultText = text.replaceAll("\\r"," ");
+                    resList.add( sr );
                 }
 
+                ArrayList<SearchResult> res = new ArrayList<>();
+                for (SearchResult sr: resList) {
+                    res.add(sr);
+                    if (res.size() >= 5) break;
+                }
+                publishResult(request, res);
+            } catch (Exception e) {
+                Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
+            } finally {
+                if (db != null){
+                    db.close();
+                }
             }
-        }.start();
+
+        }).start();
     }
 
 
