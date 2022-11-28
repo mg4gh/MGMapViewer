48f7f1eb8c6718f04746887ac3d134b16d3f9ab4
mg4gh <mg4gh@web.de>
2022-11-28 22:23:35 +0100

    rework warnings in Graphhopper

--- mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/Graphhopper.java
+++ mgmap/src/main/java/mg/mgmap/activity/mgmap/features/search/provider/Graphhopper.java
@@ -23,7 +23,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Properties;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -41,7 +40,6 @@ import mg.mgmap.activity.mgmap.features.search.SearchView;
 import mg.mgmap.generic.model.PointModel;
 import mg.mgmap.generic.model.PointModelImpl;
 import mg.mgmap.generic.util.basic.NameUtil;
-import mg.mgmap.application.util.PersistenceManager;
 
 public class Graphhopper extends SearchProvider {
 
@@ -72,79 +70,76 @@ public class Graphhopper extends SearchProvider {
 
         PointModel pm = request.pos;
 
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    ArrayList<SearchResult> resList = new ArrayList<>();
+        new Thread(() -> {
+            try {
+                ArrayList<SearchResult> resList = new ArrayList<>();
 
-                    String sUrl;
-                    if (request.text.equals("")){
-                        sUrl = String.format(Locale.ENGLISH, "%s&key=%s&point=%.6f,%.6f&limit=5&reverse=true",
-                                URL_ORS, apiKey, pm.getLat(), pm.getLon());
-                    } else {
-                        sUrl = String.format(Locale.ENGLISH, "%s&key=%s&point=%.6f,%.6f&limit=5&q=%s",
-                                URL_ORS, apiKey, pm.getLat(), pm.getLon(), request.text);
-                    }
-                    Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+sUrl);
+                String sUrl;
+                if (request.text.equals("")){
+                    sUrl = String.format(Locale.ENGLISH, "%s&key=%s&point=%.6f,%.6f&limit=5&reverse=true",
+                            URL_ORS, apiKey, pm.getLat(), pm.getLon());
+                } else {
+                    sUrl = String.format(Locale.ENGLISH, "%s&key=%s&point=%.6f,%.6f&limit=5&q=%s",
+                            URL_ORS, apiKey, pm.getLat(), pm.getLon(), request.text);
+                }
+                Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+sUrl);
 
-                    URL url = new URL(sUrl);
-                    URLConnection conn = url.openConnection();
-                    InputStream is = conn.getInputStream();
+                URL url = new URL(sUrl);
+                URLConnection conn = url.openConnection();
+                InputStream is = conn.getInputStream();
 
-                    JsonReader jsonReader = Json.createReader(is);
-                    JsonObject oAll = jsonReader.readObject();
+                JsonReader jsonReader = Json.createReader(is);
+                JsonObject oAll = jsonReader.readObject();
 
-                    JsonArray fAll = oAll.getJsonArray("hits");
-                    for (JsonValue f : fAll) {
+                JsonArray fAll = oAll.getJsonArray("hits");
+                for (JsonValue f : fAll) {
 
-                        try {
-                            JsonObject fo = f.asJsonObject();
+                    try {
+                        JsonObject fo = f.asJsonObject();
 
-                            String res = "";
-                            String key;
+                        String res = "";
+                        String key;
 
-                            key="name";
-                            res += (!fo.containsKey(key))?"":fo.getString(key);
+                        key="name";
+                        res += (!fo.containsKey(key))?"":fo.getString(key);
 
-                            key="street";
-                            res += (!fo.containsKey(key))?"":(", "+fo.getString(key));
+                        key="street";
+                        res += (!fo.containsKey(key))?"":(", "+fo.getString(key));
 
-                            key="housenumber";
-                            res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
+                        key="housenumber";
+                        res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
 
-                            res += ",";
-                            key="postcode";
-                            res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
+                        res += ",";
+                        key="postcode";
+                        res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
 
-                            key="city";
-                            res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
+                        key="city";
+                        res += (!fo.containsKey(key))?"":(" "+fo.getString(key));
 
-                            if (res.endsWith(",")){
-                                res = res.substring(0,res.length()-2);
-                            }
-
-                            JsonObject jpo =  fo.getJsonObject("point");
-
-                            double lon = jpo.getJsonNumber("lng").doubleValue();
-                            double lat = jpo.getJsonNumber("lat").doubleValue();
-                            PointModel pm = new PointModelImpl(lat,lon);
-
-                            resList.add( new SearchResult(request, res, pm));
-                            Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+res);
-                        } catch (Exception e) {
-                            Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
+                        if (res.endsWith(",")){
+                            res = res.substring(0,res.length()-2);
                         }
 
+                        JsonObject jpo =  fo.getJsonObject("point");
+
+                        double lon = jpo.getJsonNumber("lng").doubleValue();
+                        double lat = jpo.getJsonNumber("lat").doubleValue();
+                        PointModel pm1 = new PointModelImpl(lat,lon);
+
+                        resList.add( new SearchResult(request, res, pm1));
+                        Log.i(MGMapApplication.LABEL, NameUtil.context()+" "+res);
+                    } catch (Exception e) {
+                        Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
                     }
 
-                    publishResult(request, resList);
-                } catch (IOException e) {
-                    Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
                 }
 
+                publishResult(request, resList);
+            } catch (IOException e) {
+                Log.e(MGMapApplication.LABEL, NameUtil.context(), e);
             }
-        }.start();
+
+        }).start();
     }
 
 
