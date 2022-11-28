fb45841485e06ee4422694dd6119b1002b8afc63
mg4gh <mg4gh@web.de>
2022-11-28 13:29:42 +0100

    fix bestMatch.getTrackLog delivers null

--- mgmap/src/main/java/mg/mgmap/activity/mgmap/MGMapActivity.java
+++ mgmap/src/main/java/mg/mgmap/activity/mgmap/MGMapActivity.java
@@ -668,17 +668,19 @@ public class MGMapActivity extends MapViewerBase implements XmlRenderThemeMenuCa
                         bestMatch = currentMatch;
                     }
                 }
-                if ((bestMatch.getTrackLog() == application.availableTrackLogsObservable.selectedTrackLogRef.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())){
-                    prefCache.get(R.string.FSATL_pref_stlGl, false).toggle();
-                    return true;
-                }
-                if ((bestMatch.getTrackLog() == application.recordingTrackLogObservable.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())) {
-                    prefCache.get(R.string.FSRecording_pref_rtlGl, false).toggle();
-                    return true;
-                }
-                if ((bestMatch.getTrackLog() == application.routeTrackLogObservable.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())){
-                    prefCache.get(R.string.FSRouting_pref_RouteGL, false).toggle();
-                    return true;
+                if (bestMatch.getTrackLog() != null){
+                    if ((bestMatch.getTrackLog() == application.availableTrackLogsObservable.selectedTrackLogRef.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())){
+                        prefCache.get(R.string.FSATL_pref_stlGl, false).toggle();
+                        return true;
+                    }
+                    if ((bestMatch.getTrackLog() == application.recordingTrackLogObservable.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())) {
+                        prefCache.get(R.string.FSRecording_pref_rtlGl, false).toggle();
+                        return true;
+                    }
+                    if ((bestMatch.getTrackLog() == application.routeTrackLogObservable.getTrackLog()) && (bestMatch.getTrackLog().hasGainLoss())){
+                        prefCache.get(R.string.FSRouting_pref_RouteGL, false).toggle();
+                        return true;
+                    }
                 }
                 return super.onLongPress(point);
             }
