8b6c7d983c7da9e664568bdee36a4c6af9675a02
mg4gh <mg4gh@web.de>
2022-11-30 17:41:49 +0100

    rework warning in EditPref

--- mgmap/src/main/java/mg/mgmap/generic/util/EditPref.java
+++ mgmap/src/main/java/mg/mgmap/generic/util/EditPref.java
@@ -5,10 +5,9 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.DatePicker;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 @SuppressLint("ViewConstructor")
 public class EditPref<T> extends androidx.appcompat.widget.AppCompatEditText{
@@ -25,19 +24,16 @@ public class EditPref<T> extends androidx.appcompat.widget.AppCompatEditText{
 
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                System.out.println("bef "+charSequence+" "+i+" "+i1+" "+i2);
                 old = charSequence.toString();
                 if (pref.getValue() instanceof Calendar){
-                    calOk = ((i2==10)); // cal my only be changed by calendar picker
+                    calOk = ((i2==10)); // call my only be changed by calendar picker
                 }
             }
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                System.out.println("chg "+charSequence+" "+i+" "+i1+" "+i2);
-            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
             @Override
             public void afterTextChanged(Editable editable) {
-//                System.out.println("aft "+editable.toString());
                 if (getText() != null) {
                     if ((!calOk) || (pref.verify(getText().toString()) == null)) {
                         setText(old);
@@ -47,35 +43,26 @@ public class EditPref<T> extends androidx.appcompat.widget.AppCompatEditText{
         });
 
         if (pref.getValue() instanceof Calendar) {
+            @SuppressWarnings("unchecked")
             Pref<Calendar> prefC = (Pref<Calendar>)pref;
             setText( pref.sdf.format(prefC.getValue().getTime()) );
 
-            setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                @Override
-                public void onFocusChange(View view, boolean focused) {
-                    if (focused){
-                        callOnClick();
-                    }
+            setOnFocusChangeListener((view, focused) -> {
+                if (focused){
+                    callOnClick();
                 }
             });
-            setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Calendar c = prefC.verify( getText().toString() ); // get current value to calendar format
-                    DatePickerDialog datePickerDialog = new DatePickerDialog(
-                            context,
-                            new DatePickerDialog.OnDateSetListener() {
-                                @Override
-                                public void onDateSet(DatePicker view, int year,
-                                                      int monthOfYear, int dayOfMonth) {
-                                    Calendar calendar = Calendar.getInstance();
-                                    calendar.set( year, monthOfYear, dayOfMonth);
-                                    setText( pref.sdf.format(calendar.getTime()) );
-                                }
-                            },
-                            c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
-                    datePickerDialog.show();
-                }
+            setOnClickListener(v -> {
+                Calendar c = prefC.verify( Objects.requireNonNull(getText()).toString() ); // get current value to calendar format
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        context,
+                        (view, year, monthOfYear, dayOfMonth) -> {
+                            Calendar calendar = Calendar.getInstance();
+                            calendar.set( year, monthOfYear, dayOfMonth);
+                            setText( pref.sdf.format(calendar.getTime()) );
+                        },
+                        c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
             });
         } else {
             setText( pref.getValue().toString() );
