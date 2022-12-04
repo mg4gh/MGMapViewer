3298486a685f09f97daa6128d51832daed7c0d37
mg4gh <mg4gh@web.de>
2022-12-04 18:55:24 +0100

    rework comments in GnssLocationListener

--- mgmap/src/main/java/mg/mgmap/generic/util/basic/IOUtil.java
+++ mgmap/src/main/java/mg/mgmap/generic/util/basic/IOUtil.java
@@ -1,18 +1,22 @@
 package mg.mgmap.generic.util.basic;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import mg.mgmap.application.MGMapApplication;
 
 public class IOUtil {
 
     public static void copyStreams(InputStream is, OutputStream os) throws IOException {
         byte[] buffer = new byte[8 * 1024];
-        int bytesRead;
-        while ((bytesRead = is.read(buffer)) != -1) {
-            os.write(buffer, 0, bytesRead);
+        try (is; os){
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
         }
-        is.close();
-        os.close();
     }
 }
