7399d33f52b847f32d961f131b303d73338449b5
mg4gh <mgroeger1@web.de>
2023-01-25 18:04:38 +0100

    fix logging on device

--- mgmap/src/main/java/mg/mgmap/application/MGMapApplication.java
+++ mgmap/src/main/java/mg/mgmap/application/MGMapApplication.java
@@ -114,7 +114,7 @@ public class MGMapApplication extends Application {
 
     public void startLogging(File logDir){
         try {
-            String cmd = "logcat "+ LABEL+":i *:W -f "+logDir.getAbsolutePath()+"/log.txt -r 10000 -n10";
+            String cmd = "logcat *:"+(BuildConfig.DEBUG?"d":"i")+" mg.mgmap:E -f "+logDir.getAbsolutePath()+"/log.txt -r 10000 -n10";
             mgLog.i("Start Logging: "+cmd);
             pLogcat = Runtime.getRuntime().exec(cmd);
         } catch (IOException e) {
