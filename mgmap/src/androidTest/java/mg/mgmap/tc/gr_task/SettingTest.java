570c4504e77b2afec0198072320f4d12e1129609
mg4gh <mgroeger1@web.de>
2023-03-04 10:49:47 +0100

    settings tests

--- mgmap/src/androidTest/java/mg/mgmap/tc/gr_task/SettingTest.java
+++ mgmap/src/androidTest/java/mg/mgmap/tc/gr_task/SettingTest.java
@@ -2,8 +2,8 @@ package mg.mgmap.tc.gr_task;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+//import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+//import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.AllOf.allOf;
 
@@ -11,11 +11,12 @@ import android.os.SystemClock;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.contrib.RecyclerViewActions;
+//import androidx.test.espresso.action.ViewActions;
+//import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -50,7 +51,41 @@ public class SettingTest extends BaseTestCase {
 
 
     @Test(timeout = 20000)
-    public void _01_settings_test() {
+    public void _01_settings_layers() {
+        mgLog.i("started");
+        MGMapActivity mgMapActivity = waitForActivity(MGMapActivity.class);
+        mgMapActivity.runOnUiThread(() -> {
+            MapPosition mp = new MapPosition(new LatLong(54.315814,13.351981), (byte) 15);
+            mgMapActivity.getMapsforgeMapView().getModel().mapViewPosition.setMapPosition(mp);
+        });
+        SystemClock.sleep(2000);
+
+        setCursorToCenterPos();
+        addRegex(".*onClick mi_settings.*");
+        animateToViewAndClick(R.id.menu_task);
+        animateToViewAndClick(R.id.mi_settings);
+        Assert.assertEquals("none", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.Layers_pref_chooseMap3_key),""));
+
+// keep this sample code in comment, just in case it will be needed later ... for whatever reason
+//        onView(withId(androidx.preference.R.id.recycler_view))
+//                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(R.string.preference_choose_theme_title)), ViewActions.click()));
+        animateToPrefAndClick(R.string.Layers_pref_chooseMap_key);
+        addRegex(".*onClick.*key=SelectMap3.*");
+        animateToPrefAndClick(R.string.Layers_pref_chooseMap3_key);
+
+        onView(allOf(withText("MAPGRID: hgt"))).perform(click());
+        Assert.assertEquals("MAPGRID: hgt", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.Layers_pref_chooseMap3_key),""));
+        SystemClock.sleep(2000);
+
+        Espresso.pressBack();
+        SystemClock.sleep(2000);
+        mgLog.i("finished");
+    }
+
+
+
+    @Test(timeout = 20000)
+    public void _02_settings_theme() {
         mgLog.i("started");
         MGMapActivity mgMapActivity = waitForActivity(MGMapActivity.class);
         mgMapActivity.runOnUiThread(() -> {
@@ -64,16 +99,59 @@ public class SettingTest extends BaseTestCase {
         animateToViewAndClick(R.id.menu_task);
         animateToViewAndClick(R.id.mi_settings);
         AppCompatActivity settingsActivity = waitForActivity(SettingsActivity.class);
-        animateTo(getCenterPos());
 
-        onView(withId(androidx.preference.R.id.recycler_view))
-                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(R.string.preference_choose_theme_title)), ViewActions.click()));
+        Assert.assertEquals("Elevate.xml", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_theme_key),""));
 
+        animateToPrefAndClick(R.string.preference_choose_theme_key);
         onView(allOf(withText("Elements.xml"))).perform(click());
+        Assert.assertEquals("Elements.xml", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_theme_key),""));
         SystemClock.sleep(2000);
-        PreferenceUtil.setPreference(settingsActivity, R.string.preference_choose_theme_key, "Elevate.xml");
 
+        PreferenceUtil.setPreference(settingsActivity, R.string.preference_choose_theme_key, "Elevate.xml");
+        SystemClock.sleep(200);
+        Assert.assertEquals("Elevate.xml", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_theme_key),""));
         SystemClock.sleep(2000);
+
+        Espresso.pressBack();
+        SystemClock.sleep(2000);
+        mgLog.i("finished");
+    }
+
+
+    @Test(timeout = 20000)
+    public void _03_settings_search() {
+        mgLog.i("started");
+        MGMapActivity mgMapActivity = waitForActivity(MGMapActivity.class);
+        mgMapActivity.runOnUiThread(() -> {
+            MapPosition mp = new MapPosition(new LatLong(54.315814,13.351981), (byte) 15);
+            mgMapActivity.getMapsforgeMapView().getModel().mapViewPosition.setMapPosition(mp);
+        });
+        SystemClock.sleep(2000);
+
+        setCursorToCenterPos();
+        addRegex(".*onClick mi_settings.*");
+        animateToViewAndClick(R.id.menu_task);
+        animateToViewAndClick(R.id.mi_settings);
+        AppCompatActivity settingsActivity = waitForActivity(SettingsActivity.class);
+
+        Assert.assertEquals("Graphhopper", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_search_key),""));
+
+
+        animateToPrefAndClick(R.string.preference_choose_search_key);
+        onView(allOf(withText("POI"))).perform(click());
+        Assert.assertEquals("POI", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_search_key),""));
+        SystemClock.sleep(1000);
+
+        PreferenceUtil.setPreference(settingsActivity, R.string.preference_choose_search_key, "Nominatim");
+        SystemClock.sleep(200);
+        Assert.assertEquals("Nominatim", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_search_key),""));
+        SystemClock.sleep(1000);
+
+        animateToPrefAndClick(R.string.preference_choose_search_key);
+        onView(allOf(withText("GeoLatLong"))).perform(click());
+        Assert.assertEquals("GeoLatLong", mgMapApplication.getSharedPreferences().getString(mgMapApplication.getResources().getString(R.string.preference_choose_search_key),""));
+        SystemClock.sleep(2000);
+
         Espresso.pressBack();
         SystemClock.sleep(2000);
         mgLog.i("finished");
