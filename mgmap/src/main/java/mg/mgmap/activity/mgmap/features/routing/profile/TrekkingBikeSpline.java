c2b36e92c146406cd3f7442db441b6b4fda721e1
Heiko Gerwens <heiko.gerwens@gmail.com>
2024-09-14 21:48:07 +0200

    Cubic Spline Cost Function Trekking Bike first version

---
+++ mgmap/src/main/java/mg/mgmap/activity/mgmap/features/routing/profile/TrekkingBikeSpline.java
@@ -0,0 +1,28 @@
+package mg.mgmap.activity.mgmap.features.routing.profile;
+
+import mg.mgmap.R;
+import mg.mgmap.activity.mgmap.features.routing.CostCalculator;
+import mg.mgmap.activity.mgmap.features.routing.RoutingProfile;
+import mg.mgmap.generic.graph.WayAttributs;
+
+public class TrekkingBikeSpline extends RoutingProfile {
+    public TrekkingBikeSpline( ) {
+        super(new CostCalcSpline( ));
+    }
+
+    protected CostCalculator getCostCalculator(CostCalculator profileCalculator, WayAttributs wayAttributs) {
+        return new CostCalcSplineTreckingBike(wayAttributs, (CostCalcSpline) profileCalculator);
+    }
+    @Override
+    public int getIconIdActive() {
+        return R.drawable.rp_trekking_a;
+    }
+    @Override
+    protected int getIconIdInactive() {
+        return R.drawable.rp_trekking_i;
+    }
+    protected int getIconIdCalculating() {
+        return R.drawable.rp_trekking_c;
+    }
+
+}
